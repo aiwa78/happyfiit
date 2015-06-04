d1efc933cf03115f69c64679a443728a51332801
Dimitris A <aiwa78@gmail.com>
2015-06-05 01:10:54 +0300

    team, map, how it works

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 root 'pages#home'
 get "about"=>"pages#about"
+get "how_it_works"=>"pages#how_it_works"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
